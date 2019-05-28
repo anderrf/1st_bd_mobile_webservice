3af56bd90ff99051a10953d71688ab412deac676
Anderson <andersonrochafarias@gmail.com>
2019-05-28 17:18:11 +0000

    tentativa lista 2

---
+++ lista.php
@@ -0,0 +1,31 @@
+<?php
+    try{
+        $conexao = mysqli_connect("localhost", "andersonrf", "", "bd_mobile");
+                                    //Servidor  usuário     senha   banco
+        
+        $query = "SELECT * FROM tb_pessoa ORDER BY cd_pessoa ASC";
+        
+        $resultado = mysqli_query($conexao, $query);
+        
+        $registro = array(
+            'pessoas'=>array()
+        );
+        
+        $i = 0;
+        
+        while($linha = mysqli_fetch_assoc($resultado)){
+            $registro['pessoas'][$i] = array(
+                'codigo' => $linha['cd_pessoa'],
+                'nome' => $linha['nm_pessoa'],
+                'email' => $linha['ds_email'],
+            );
+            $i++;
+        }
+
+        echo json_encode($registro);
+        
+    }catch (Exception $e){
+        
+        echo "Falha: " . $e;
+        
+    }
---
+++ registra.php
@@ -0,0 +1,16 @@
+<?php
+    try{
+        $conexao = mysqli_connect("localhost", "andersonrf", "", "bd_mobile");
+                                    //Servidor  usuário     senha   banco
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $query = "INSERT INTO tb_pessoa VALUES (NULL ,  '$nome',  '$email');";
+        mysqli_query($conexao, $query);
+        
+        echo "Cadastro realizado com sucesso";
+        
+    }catch (Exception $e){
+        
+        echo "Falha: " . $e;
+        
+    }
